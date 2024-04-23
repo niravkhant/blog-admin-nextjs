c243fea5c571d4edbdc21b0a8cb01153922dc1f2
Nirav Khant <29600493+niravkhant@users.noreply.github.com>
2024-04-23 21:05:44 +0530

    Versioning untracked files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8001/api/v1";
-// export const BASE_URL = "https://appcoreservices.tech/api/v1";
+// export const BASE_URL = "http://localhost:8001/api/v1";
+export const BASE_URL = "https://appcoreservices.tech/api/v1";
